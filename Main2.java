5d130dd5d85a45ba961bfa555888709cada03ae6
fuelmine <99046949+fuelmine@users.noreply.github.com>
2024-04-09 16:10:16 -0500

    Create Main2.java commit 3

---
+++ Main2.java
@@ -0,0 +1,14 @@
+public Main2{
+  private int data = 0;
+
+  public Main2(){
+  }
+  public Main2(int u){
+    this.data = u;
+  }
+
+  public void printData(){
+    System.out.println(this.data);
+  }
+
+}
